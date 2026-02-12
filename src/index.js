d9b64100baa85c60813b1f9d8e4c737f7b5613bc
Ishan Kesharwani <ishankesharwani@Ishans-MacBook-Air.local>
2026-02-12 11:05:46 +0530

    Shifting to jet brains

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchRouter } from './routes/matches';
 
 const app = express();
 const PORT = 8000;
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Express server!' });
 });
 
+app.use('/matches', matchRouter)
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
