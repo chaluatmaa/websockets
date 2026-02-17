15093df31db983bcb77509bb4b102ebf35c09eae
Ishan Kesharwani <ishankesharwani@Ishans-MacBook-Air.local>
2026-02-17 17:40:19 +0530

    implemented websocket server.

--- src/ws/server.js
+++ src/ws/server.js
@@ -23,9 +23,20 @@ export function attachWebSocketServer(server) {
     });
 
     wss.on('connection', (socket) => {
+        socket.isAlive = true;
+        socket.on('pong', () => (socket.isAlive = true));
         sendJson(socket, { type: 'Welcome' });
         socket.on('error', (err) => console.error('WebSocket error:', err));
     })
+
+    const interval = setInterval(() => wss.clients.forEach((ws) => {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping(() => {});
+    }), 30000)
+
+    wss.on('close', () => clearInterval(interval));
 
     function broadcastMatchCreated(match) {
         broadcast(wss, { type: 'match_created', data: match });
